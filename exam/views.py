e63381a048e8384b7049557b99011035e1dd4dbb
saikrishna7004 <74298585+saikrishna7004@users.noreply.github.com>
2021-07-29 17:44:23 +0530

    Update views.py

--- exam/views.py
+++ exam/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponse, redirect
 import json, time, os
 from .models import Question, UserData
 from django.db import transaction
-from examproject.settings import STATICFILES_DIR
+from examproject.settings import STATICFILES_DIRS
 
 # Create your views here.
 
@@ -142,7 +142,7 @@ def create(request):
 		return HttpResponse("<meta name='viewport' content='width=device-width, initial-scale=1.0'><h2>You are not permitted to access this page. Login as a Staff or SuperUser to access this page.</h2><br><a href='/login'>Click Here</a> to redirect to Login page.")
 	import time
 	start=time.time()
-	question_paper = open(os.path.join(STATICFILES_DIR,"/static/examtest.txt"),'r').read()
+	question_paper = open(os.path.join(STATICFILES_DIRS[0],"/static/examtest.txt"),'r').read()
 	question_set = question_paper.split("\n__________\n")
 	exam_id = 5056
 	choice_id_inc = str(exam_id)+'000'
