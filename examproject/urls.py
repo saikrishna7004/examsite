46e50135894f3be24114f164f9da83bb8eea1e18
saikrishna7004 <74298585+saikrishna7004@users.noreply.github.com>
2021-08-09 12:42:10 +0530

    Add files via upload

--- examproject/urls.py
+++ examproject/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
     path('', include("home.urls")),
     path('admin/', admin.site.urls),
     path('exam/', include("exam.urls")),
+    path('favicon.ico', RedirectView.as_view(url='/static/home/favicon.ico')),
 ]
