fb4098ed09be2ad24aea281735c26edda4e55c65
saikrishna7004 <74298585+saikrishna7004@users.noreply.github.com>
2022-11-28 23:24:03 +0530

    Modified build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'pass')" | python manage.py shell
